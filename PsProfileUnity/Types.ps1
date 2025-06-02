3438b28f4e603d3ec9f792df546e4f1b5048bf2b
Arend Wevers <47255711+ArendWevers@users.noreply.github.com>
2025-06-02 21:46:32 +0200

    Complete filter enums

--- PsProfileUnity/Types.ps1
+++ PsProfileUnity/Types.ps1
@@ -20,67 +20,56 @@ Enum FilterRuleType {
 
 enum FilterRuleOperator {
     Is = 0
-    IsNot = 1 # todo
-    Contains = 2 # todo
-    NotContains = 3 # todo
-    StartsWith = 2 # todo
-    EndsWith = 3 # todo
-    GreaterThan = 4 # todo
-    LessThan = 5 # todo
+    IsNot = 1
+    Contains = 2
+    NotContains = 3
+    StartsWith = 4
+    EndsWith = 5
+    Range = 6
 }
 
 [Flags()]enum FilterMachineClasses {
-    Desktop = 0 # todo
-    Laptop = 1 # todo
-    TerminalServer = 2 # todo
-    MemberServer = 4 # todo
-    DomainController = 8 # todo
-    Unknown1 = 16 # todo
-    Unknown2 = 32 # todo
+    Desktop = 16
+    Laptop = 32
+    TerminalServer = 2
+    MemberServer = 4
+    DomainController = 8
 }
 
 [Flags()]enum FilterOperatingSystems {
-    Windows8 = 1 # todo
-    Windows8_1 = 2 # todo
-    Windows2012 = 4 # todo
-    Windows2012R2 = 8 # todo
-    Windows10 = 16 # 107520  # 33792
-    Windows10_IOT = 32 # 107520  # 33792
-    Windows11 = 64 # 107520  # 73728
-    Windows11_IOT = 128 # 107520  # 73728
-    Unknown1 = 256 # todo
-    Unknown2 = 512 # todo
-    Unknown3 = 1024 # todo
+    Windows8 = 128
+    Windows8_1 = 256
+    Windows2012 = 64
+    Windows2012R2 = 512
+    Windows10 = 1024
+    Windows10_IOT = 32768
+    Windows11 = 8192
+    Windows11_IOT = 65536
     Windows2016 = 2048
     Windows2019 = 4096
-    Unknown5 = 8192 # todo
     Windows2022 = 16384
-    Unknown6 = 32768 # todo
-    Unknown7 = 65536 # todo
     Windows2025 = 131072
 }
 
 [flags()]enum FilterSystemEvents {
     LogonLogoff = 1
-    TriggerPoints = 2 # todo
+    TriggerPoints = 2
 }
 
 [flags()]enum FilterConnnections {
-    RDPSession = 1 # todo
-    ICASession = 2 # todo
-    PCoIPSession = 4 # todo
-    ConsoleSession = 8 # todo
-    VMwareBlast  = 16 # todo
-    AmazonAppStream = 32 # todo
-    AmazonWSP = 64 # todo
-    DizzionFrame = 128 # todo
-    Unknown1 = 256 # todo
-    Unknown2 = 512 # todo
+    RDPSession = 4
+    ICASession = 8
+    PCoIPSession = 16
+    ConsoleSession = 32
+    VMwareBlast  = 64
+    AmazonAppStream = 128
+    AmazonWSP = 512
+    DizzionFrame = 256
 }
 
 enum FilterRuleAggregate{
     And = 0
-    Or = 1 # todo
+    Or = 1
 }
 
 class FilterRule {
